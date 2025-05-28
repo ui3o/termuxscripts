480c0b886fe328dc741d782cf9ffdd31fb9262aa
ui3o <43638725+ui3o@users.noreply.github.com>
2025-05-28 23:11:06 +0000

    demo.sh fix

--- demo.sh
+++ demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -n $DEMOSH ] || echo demo.sh from home called && exit 1
+[[ -n $DEMOSH ]] && exit 1
 export DEMOSH=1
 [[ ! -d "~/termuxscripts" ]] && git clone https://github.com/ui3o/termuxscripts.git
 cd ~/termuxscripts
